a2f735fce6774dc41e9242f80d70c2eb3c956790
Martins Osmanis <martinosmanis@gmail.com>
2019-01-10 16:53:18 +0200

    Initial commit

---
+++ Binaru_Skaitlu_Spele.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:47:07 01/10/2019 
+// Design Name: 
+// Module Name:    Binaru_Skaitlu_Spele 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Binaru_Skaitlu_Spele(
+    );
+
+
+endmodule
